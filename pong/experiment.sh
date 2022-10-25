81afb18e56b6aa1c818481f5aa424f1787d2d4e8
Theomat <theomatricon@gmail.com>
2022-10-25 14:16:12 +0200

    Added part of pong

---
+++ pong/experiment.sh
@@ -0,0 +1,4 @@
+#!usr/bin/bash
+FILE_NAME="pong_dqn.pt"
+python generate_policy.py $FILE_NAME --env "ALE/Pong-ram-v5"
+python -m polext extract.py $FILE_NAME
---
+++ pong/generate_policy.py
@@ -0,0 +1,67 @@
+import gym
+from stable_baselines3 import DQN
+from stable_baselines3.common.evaluation import evaluate_policy
+
+import argparse
+
+
+parser = argparse.ArgumentParser(
+    description="Generate a trained DQN network for a gym environment"
+)
+parser.add_argument(
+    type=str,
+    dest="file",
+    action="store",
+    help="destination file for the model",
+)
+parser.add_argument(
+    "-e",
+    "--env",
+    type=str,
+    default="ALE/Pong-ram-v5",
+    help="name of the environment (default: ALE/Pong-ram-v5)",
+)
+parser.add_argument(
+    "-t",
+    "--timesteps",
+    type=int,
+    default=int(5e5),
+    help="number of learning timesteps (default: 5e5)",
+)
+
+
+parameters = parser.parse_args()
+file: str = parameters.file
+env: str = parameters.env
+timesteps: int = parameters.timesteps
+
+
+model = model = DQN(
+    "MlpPolicy",
+    env,
+    verbose=1,
+    exploration_final_eps=0.1,
+    target_update_interval=250,
+)
+
+
+# Separate env for evaluation
+eval_env = gym.make(env)
+
+
+# Random Agent, before training
+mean_reward, std_reward = evaluate_policy(
+    model, eval_env, n_eval_episodes=10, deterministic=True
+)
+print(f"Before training mean_reward={mean_reward:.2f} +/- {std_reward}")
+
+
+# Train the agent
+model.learn(total_timesteps=timesteps)
+# Save the agent
+model.save(file)
+
+mean_reward, std_reward = evaluate_policy(
+    model, eval_env, n_eval_episodes=10, deterministic=True
+)
+print(f"After training mean_reward={mean_reward:.2f} +/- {std_reward}")
