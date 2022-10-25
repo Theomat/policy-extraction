34c5c3e0637722a4a302c815f8191560c4ef3b95
Theomat <theomatricon@gmail.com>
2022-10-25 13:53:35 +0200

    Fixed __init__ with __main__

---
+++ polext/__main__.py
@@ -0,0 +1,26 @@
+if __name__ == "__main__":
+    import argparse
+
+
+    parser = argparse.ArgumentParser(prog="polext",
+        description="Extract a Decision Tree from a DQN"
+    )
+    parser.add_argument(
+        type=str,
+        dest="script_path",
+        action="store",
+        help="path to python script for the algorithm",
+    )
+    parser.add_argument(
+        type=str,
+        dest="model_path",
+        default="",
+        help="DQN model file to load",
+    )
+
+
+    parameters = parser.parse_args()
+    script_path: str = parameters.script_path
+    model_path: str = parameters.model_path
+
+    #TODO: call method
