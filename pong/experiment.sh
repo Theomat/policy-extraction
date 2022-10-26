14c632cb4feaf7c97df5b3222f673a91b27b4ae4
Theomat <theomatricon@gmail.com>
2022-10-26 10:58:47 +0200

    Fixed env id and number of timesteps

--- pong/experiment.sh
+++ pong/experiment.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 FILE_NAME="pong_dqn.pt"
-ENV="ALE/Pong-ram-v5"
-python scripts/generate_policy.py $FILE_NAME $ENV -t 5e5
+ENV="Pong-ram-v4"
+python scripts/generate_policy.py $FILE_NAME $ENV -t 500000
 python scripts/record_video.py $FILE_NAME $ENV
 python -m polext extract.py $FILE_NAME
