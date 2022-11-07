d3cc0cc79755fe79c8f07a86b04cd9198abf0208
Theomat <theomatricon@gmail.com>
2022-11-07 12:10:48 +0100

    Refactor

--- infinite/pong/experiment.sh
+++ infinite/pong/experiment.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 FILE_NAME="pong_dqn.pt"
-ENV="Pong-ram-v4"
-python scripts/generate_policy.py $FILE_NAME $ENV -t 500000
+ENV="ALE/Pong-ram-v5"
+python scripts/generate_policy.py $FILE_NAME $ENV -t 5000000
 python scripts/record_video.py $FILE_NAME $ENV
 python -m polext extract.py $FILE_NAME
