b48e08f1ee82b17be35283e2bcabf6c35262e0e0
Theomat <theomatricon@gmail.com>
2022-11-23 15:29:52 +0100

    Deleted pong

--- infinite/pong/experiment.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-FILE_NAME="pong_dqn.pt"
-ENV="ALE/Pong-ram-v5"
-python scripts/generate_policy.py $FILE_NAME $ENV -t 5000000
-python scripts/record_video.py $FILE_NAME $ENV
-python -m polext extract.py $FILE_NAME
