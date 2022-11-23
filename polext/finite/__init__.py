e118b3abfeb17f20accd5e772d42939637b11f41
Theomat <theomatricon@gmail.com>
2022-11-23 18:21:56 +0100

    Renaemd exports

--- polext/finite/__init__.py
+++ polext/finite/__init__.py
@@ -2,7 +2,8 @@ from polext.finite.tree_builder import (
     build_tree,
     build_forest,
     _METHODS_,
-    tree_loss as finite_tree_loss,
+    tree_loss,
+    forest_loss,
 )
 
 FINITE_METHODS = list(_METHODS_.keys())
