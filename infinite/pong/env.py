df447a3013c57216fe0b09735229aeb7b49cbb73
Theomat <theomatricon@gmail.com>
2022-11-28 12:49:21 +0100

    Fixed env

--- infinite/pong/env.py
+++ infinite/pong/env.py
@@ -1,23 +1,27 @@
 from typing import Callable, List, Tuple
 import numpy as np
 from stable_baselines3 import DQN
-from stable_baselines3.common.atari_wrappers import AtariWrapper
+from stable_baselines3.common.vec_env.vec_frame_stack import VecFrameStack
+from stable_baselines3.common.env_util import make_atari_env
 
 from polext import Predicate
 import torch
-import gym
 
 
-make_env = lambda: AtariWrapper(gym.make("PongNoFrameskip-v4"))
+make_env = lambda: VecFrameStack(
+    make_atari_env("PongNoFrameskip-v4", wrapper_kwargs={"screen_size": 84}), 4, "last"
+)
 env = make_env()
 
 
 def Q_builder(path: str) -> Callable[[np.ndarray], List[float]]:
-    model = DQN("CnnPolicy", make_env())
+    model = DQN("CnnPolicy", make_env(), buffer_size=0)
     model = model.load(path)
 
     def f(observation: np.ndarray) -> List[float]:
-        observation = torch.tensor(observation, device=model.device).unsqueeze_(0)
+        observation = (
+            torch.tensor(observation, device=model.device).squeeze_(0).swapdims_(0, 2)
+        )
         with torch.no_grad():
             q_values = model.q_net(observation)[0]
         return [x.item() for x in q_values]
@@ -28,7 +32,7 @@ def Q_builder(path: str) -> Callable[[np.ndarray], List[float]]:
 predicates = [
     Predicate(
         "my paddle y < ball y",
-        lambda obs: get_paddle_y(MY_PADDLE, obs) < get_ball(obs)[1],
+        lambda obs: get_paddle_y(MY_PADDLE, obs[0]) < get_ball(obs[0])[1],
     ),
 ]
 
@@ -42,47 +46,71 @@ def get_paddle_y(paddle: int, img: np.ndarray) -> int:
     return np.argmax(img[14:76, paddle] != 87)
 
 
-def get_ball(img: np.ndarray) -> Tuple[int, int]:
-    tmp = img[14:76, ENEMY_PADDLE + 1 : MY_PADDLE] != 87
+def get_ball_pos(img: np.ndarray, index: int = 3) -> Tuple[int, int]:
+    tmp = img[14:76, ENEMY_PADDLE + 1 : MY_PADDLE, index] != 87
     x = np.max(np.argmax(tmp, axis=0, keepdims=True))
     return x, np.argmax(tmp[x])
 
 
+def get_ball_speed(img: np.ndarray) -> Tuple[int, int]:
+    dx, dy = get_ball_pos(img, 3)
+    sx, sy = get_ball_pos(img, 0)
+    return dx - sx, dy - sy
+
+
 def make_pred(val: int, getter: Callable):
     def f(obs):
-        return val <= getter(obs)
+        return val <= getter(obs[0])
 
     return f
 
 
 for i in range(ENEMY_PADDLE + 1, MY_PADDLE, 10):
     predicates.append(
-        Predicate(f"ball x < {i}", make_pred(i, lambda obs: get_ball(obs)[0]))
+        Predicate(f"ball x <= {i}", make_pred(i, lambda obs: get_ball_pos(obs)[0]))
     )
+
 for i in range(0, 62, 10):
     predicates.append(
         Predicate(
-            f"my paddle y < {i}", make_pred(i, lambda obs: get_paddle_y(MY_PADDLE, obs))
+            f"my paddle y <= {i}",
+            make_pred(i, lambda obs: get_paddle_y(MY_PADDLE, obs)),
         )
     )
     predicates.append(
         Predicate(
-            f"enemy paddle y < {i}",
+            f"enemy paddle y <= {i}",
             make_pred(i, lambda obs: get_paddle_y(ENEMY_PADDLE, obs)),
         )
     )
     predicates.append(
-        Predicate(f"ball y < {i}", make_pred(i, lambda obs: get_ball(obs)[1]))
+        Predicate(f"ball y <= {i}", make_pred(i, lambda obs: get_ball_pos(obs)[1]))
+    )
+
+for i in range(7):
+    predicates.append(
+        Predicate(f"ball vx <= {i}", make_pred(i, lambda obs: get_ball_speed(obs)[0]))
+    )
+    predicates.append(
+        Predicate(f"ball vy <= {i}", make_pred(i, lambda obs: get_ball_speed(obs)[1]))
     )
 
 # if __name__ == "__main__":
 #     env = make_env()
 #     obs = env.reset()
 #     for i in range(400):
-#         obs = env.step(0)[0]
-#     new_obs = obs[14:76] != 87
-#     print("my paddle:", get_paddle_y(MY_PADDLE, obs))
-#     print("enemy paddle:", get_paddle_y(ENEMY_PADDLE, obs))
-#     print("ball pos:", get_ball(obs))
-#     # py =
-#     # print(py)
+#         obs = env.step([0])[0].squeeze(0)
+#     print(obs.shape)
+#     for predicate in predicates:
+#         print(predicate.name, predicate(obs))
+#     from matplotlib import pyplot as plt
+#     plt.figure()
+#     for i in range(4):
+#         plt.subplot(1, 4, 1 + i)
+#         plt.imshow(obs[14:76, ENEMY_PADDLE:MY_PADDLE + 1, i])
+#     plt.show()
+# print("my paddle:", get_paddle_y(MY_PADDLE, obs))
+# print("enemy paddle:", get_paddle_y(ENEMY_PADDLE, obs))
+# print("ball pos:", get_ball(obs))
+# py =
+# print(py)
