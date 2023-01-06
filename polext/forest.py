151cd7840b6291b0741194522a774c1c3f694315
Theomat <theomatricon@gmail.com>
2023-01-06 09:44:09 +0100

    Updated forest to be coherent with trees

--- polext/forest.py
+++ polext/forest.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
-from typing import Dict, Generic, Iterable, TypeVar
+from typing import Dict, Generic, Iterable, Optional, Tuple, TypeVar, Union
 
+from polext.predicate_space import PredicateSpace
 from polext.decision_tree import DecisionTree
 
 S = TypeVar("S")
@@ -10,14 +11,41 @@ class Forest(Generic[S]):
     def __init__(self, trees: Iterable[DecisionTree[S]]) -> None:
         self.trees = list(trees)
 
-    def __call__(self, state: S) -> int:
+    def eval(self, state: S) -> int:
         return majority_vote(self.votes(state))
+
+    def eval_pred_space(self, state: Tuple[bool, ...], space: PredicateSpace[S]) -> int:
+        return majority_vote(self.votes_pred_space(state, space))
+
+    def __call__(
+        self,
+        state: Union[S, Tuple[bool, ...]],
+        space: Optional[PredicateSpace[S]] = None,
+    ) -> int:
+        if space is None:
+            return self.eval(state)  # type: ignore
+        return self.eval_pred_space(state, space)  # type: ignore
+
+    def __call__(self, state: S) -> int:
+        return
 
     def votes(self, state: S) -> Dict[int, int]:
         votes = defaultdict(int)
         for tree in self.trees:
-            votes[tree(state)] += 1
+            votes[tree.eval(state)] += 1
         return votes
+
+    def votes_pred_space(
+        self, state: Tuple[bool, ...], space: PredicateSpace[S]
+    ) -> Dict[int, int]:
+        votes = defaultdict(int)
+        for tree in self.trees:
+            votes[tree.eval_pred_space(state, space)] += 1
+        return votes
+
+    def seed(self, seed: Optional[int]) -> None:
+        for i, tree in enumerate(self.trees):
+            tree.seed(seed + i)
 
 
 def majority_vote(votes: Dict[int, int]) -> int:
